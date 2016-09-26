b879ae46a49d1f1442ce7ac32dfe8a07106df2c2
huangwenjian <631236933@qq.com>
2016-09-26 13:34:30 +0800

    modify

---
+++ app/src/main/java/com/example/huangwenjian/myleanchatdemo1/activity/PictureSelectActivity.java
@@ -0,0 +1,29 @@
+package com.example.huangwenjian.myleanchatdemo1.activity;
+
+import com.example.huangwenjian.myleanchatdemo1.R;
+import com.example.huangwenjian.myleanchatdemo1.base.BaseActivity;
+
+/**
+ * 作者: huangwenjian
+ * <p>
+ * 描述:
+ * <p>
+ * 时间: 16/9/26
+ */
+
+public class PictureSelectActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+        setContentView(R.layout.activity_picture_select);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+}
